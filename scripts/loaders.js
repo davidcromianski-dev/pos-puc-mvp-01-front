afc5f4ea78ea72d1a2c2d465122a73c0c0a89f75
David Cromianski <142181851+davidcromianski-dev@users.noreply.github.com>
2025-07-06 22:04:41 -0300

    Update loaders.js
    Removed unnecessary comments

--- scripts/loaders.js
+++ scripts/loaders.js
@@ -7,11 +7,6 @@ class BaseLoader {
 class ShoelaceLoader extends BaseLoader {
     static async load() {
         await Promise.race([
-            Promise.allSettled([
-                // customElements.whenDefined('sl-button'),
-                // customElements.whenDefined('sl-card'),
-                // customElements.whenDefined('sl-rating')
-            ]),
             new Promise(resolve => setTimeout(resolve, 2000))
         ]);
         document.documentElement.classList.remove('reduce-fouce');
